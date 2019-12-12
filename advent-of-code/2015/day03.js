c5857611098c5aa3276e6bb0d925e3e1f918e6cc
Richard Zurad <rzurad@gmail.com>
2019-12-11 23:57:41 -0800

    We've struck gold here

--- advent-of-code/2015/day03.js
+++ advent-of-code/2015/day03.js
@@ -2,13 +2,21 @@
 
 const fs = require('fs');
 
-const coord = {
-    x: 0,
-    y: 0,
-    toString() { return `${this.x}x${this.y}`; }
-};
+class Coord {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+    }
 
-const map = { [coord]: 1 };
+    toString() {
+        return `${this.x}x${this.y}`;
+    }
+}
+
+const scoord = new Coord();
+const rcoord = new Coord();
+
+const map = { [scoord]: 2 };
 
 function isValidMove(move) {
     return move === '>' || move === '<' || move === '^' || move === 'v';
@@ -19,6 +27,7 @@ fs.readFile('./day03.data', 'utf8', (err, data) => {
 
     for (let i = 0; i < data.length; i++) {
         const move = data[i];
+        const coord = i % 2 ? scoord : rcoord;
 
         if (isValidMove(move)) {
             switch (move) {
